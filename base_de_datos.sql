6f5a0e52aa1600149d8983bf471d28976541ef6d
efren <efrenpgc2602@gmail.com>
2022-09-25 23:04:06 -0500

    tablas auditoria

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -8,6 +8,7 @@ drop table if exists categoria;
 create or replace table categoria (
     id_cate int(11) not null auto_increment,
     descripcion varchar(255) not null,
+    estado int not null,
     primary key (id_cate)
 );
 
@@ -17,6 +18,7 @@ create or replace table sub_categorias (
     id_sub_cate int(11) not null auto_increment,
     fk_categoria int(11) not null,
     descripcion varchar(255) not null,
+    estado int not null,
 	constraint fk_categoria_sub_categoria foreign key (fk_categoria) 
 	references categoria (id_cate),
 	primary key (id_sub_cate)
@@ -28,6 +30,7 @@ create or replace table unidad_producto(
   id_unid_prod int(11) not null auto_increment,
   sigla varchar(5) not null,
   descripcion varchar(255) not null,
+  estado int not null,
   primary key (id_unid_prod)
 );
 
@@ -36,6 +39,7 @@ drop table if exists catalogo_producto;
 create or replace table catalogo_producto (
     id_cata_prod int(11) not null auto_increment,
     descripcion varchar(255) not null,
+    estado int not null,
     primary key (id_cata_prod)
 );
 
@@ -43,7 +47,8 @@ create or replace table catalogo_producto (
 drop table if exists area_almacenado;
 create or replace table area_almacenado(
 	id_almacenado int not null auto_increment,
-	area varchar(20) not null,
+	descripcion varchar(50) not null,
+	estado int not null,
 	primary key (id_almacenado)
 );
 
@@ -53,6 +58,7 @@ create or replace table area_almacenado_producto (
     id_alma_prod int(11) not null auto_increment,
     fk_cata_prod int(11) not null,
     fk_area_alma int(11) not null,
+    estado int not null,
     constraint area_almacenado_producto_ibfk_1 foreign key (fk_cata_prod) 
     references catalogo_producto (id_cata_prod),
     constraint area_almacenado_producto_ibfk_2 foreign key (fk_area_alma) 
@@ -68,6 +74,7 @@ create or replace table presentacion_catalogo_producto (
     fk_sub_cate int(11) not null,
     cantidad double not null,
     fk_unid_prod int(11) not null,
+    estado int not null,
     constraint fk_cata_prod_pres_cata_prod foreign key (fk_cata_prod) 
     references catalogo_producto(id_cata_prod),
     constraint fk_sub_cate_pres_cata_prod foreign key (fk_sub_cate) 
@@ -84,6 +91,7 @@ create or replace table precio_catalogo_producto (
     fk_pres_cata_prod int(11) not null,
     fk_unid_prod int(11) not null,
     precio double(10, 2) not null,
+    estado int not null,
     constraint fk_pres_cata_prod_prec_cata_prod foreign key (fk_pres_cata_prod) 
     references presentacion_catalogo_producto (id_pres_cata_prod),
     constraint fk_unid_prod_prec_cata_prod foreign key(fk_unid_prod) 
@@ -162,7 +170,7 @@ create or replace table det_factura(
 drop table if exists cliente_auditoria;
 create or replace table cliente_auditoria(
 	id_cliente_auditoria int not null auto_increment,
-	fk_cliente int not null,
+	id_cliente int not null,
 	cedula varchar(11) not null,
 	nombre varchar(50) not null,
 	contacto varchar(50) not null,
@@ -173,5 +181,163 @@ create or replace table cliente_auditoria(
 );
 
 
+drop table if exists det_factura_auditoria;
+create or replace table det_factura_auditoria(
+	id_det_factura_auditoria int not null auto_increment,
+	id_det_factura int not null,
+	fk_cb_factura int not null,
+	fk_producto int not null,
+	total decimal(10, 2) not null,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_det_factura_auditoria)
+);
 
 
+drop table if exists det_pedido_auditoria;
+create or replace table det_pedido_auditoria(
+	id_det_pedido_auditoria int not null auto_increment,
+	id_det_pedido int not null,
+	fk_cb_factura int not null,
+	fk_producto int not null,
+	cantidad int not null,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_det_pedido_auditoria)
+);
+
+
+drop table if exists precio_catalogo_producto_auditoria;
+create or replace table precio_catalogo_producto_auditoria(
+	id_prec_cata_prod_auditoria int not null auto_increment,
+	id_prec_cata_prod int not null,
+	fk_pres_cata_prod int not null,
+	fk_unid_prod int not null,
+	precio decimal(12, 2) not null,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_prec_cata_prod_auditoria)
+);
+
+
+drop table if exists presentacion_catalogo_producto_auditoria;
+create or replace table presentacion_catalogo_producto_auditoria(
+	id_pres_cata_prod_auditoria int not null auto_increment,
+	id_pres_cata_prod int not null,
+	fk_cata_prod int not null,
+	fk_sub_cate int not null,
+	cantidad int not null,
+	fk_unid_prod int not null,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_pres_cata_prod_auditoria)
+);
+
+
+drop table if exists sub_categoria_auditoria;
+create or replace table sub_categoria_auditoria(
+	id_sub_cate_auditoria int not null auto_increment,
+	id_sub_cate int not null,
+	fk_categoria int not null,
+	descripcion varchar(255),
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_sub_cate_auditoria)
+);
+
+
+drop table if exists unidad_producto_auditoria;
+create or replace table unidad_producto_auditoria(
+	id_unid_prod_auditoria int not null auto_increment,
+	id_unid_prod int not null,
+	sigla int not null,
+	descripcion varchar(255),
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_unid_prod_auditoria)
+);
+
+
+-- GIANPIER
+
+
+drop table if exists area_almacenado_auditoria;
+create or replace table area_almacenado_auditoria(
+	id_area_almacenado_auditoria int not null auto_increment,
+	id_almacenado int not null,
+	descripcion varchar(20) not null,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key (id_area_almacenado_auditoria)
+);
+
+
+drop table if exists area_almacenado_producto_auditoria;
+create or replace table area_almacenado_producto_auditoria(
+	id_area_almacenado_producto_auditoria int not null auto_increment,
+    id_alma_prod int(11) not null,
+    fk_cata_prod int(11) not null,
+    fk_area_alma int(11) not null,
+    estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key (id_area_almacenado_producto_auditoria)
+);
+
+
+drop table if exists cab_factura_auditoria;
+create or replace table cab_factura_auditoria(
+	id_cab_factura_auditoria int not null auto_increment,
+	id_cab_factura int not null,
+	fk_cab_pedido int not null,
+	sub_total decimal(10, 2) not null,
+ 	total decimal(10, 2) not null,
+	fecha_creacion timestamp default current_timestamp,
+	estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_cab_factura_auditoria)
+);
+
+
+drop table if exists cab_pedido_auditoria;
+create or replace table cab_pedido_auditoria(
+	id_cab_pedido_auditoria int not null auto_increment,
+	id_cab_pedido int not null,
+	fk_cliente int not null,
+	estado int  not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_cab_pedido_auditoria)
+);
+
+
+drop table if exists catalogo_producto_auditoria;
+create or replace table catalogo_producto_auditoria(
+	id_catalogo_producto_auditoria int not null auto_increment,
+    id_cata_prod int(11) not null,
+    descripcion varchar(255) not null,
+    estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_catalogo_producto_auditoria)
+);
+
+
+drop table if exists categoria_auditoria;
+create or replace table categoria_auditoria(
+	id_categoria_auditoria int not null auto_increment,
+    id_cate int(11) not null,
+    descripcion varchar(255) not null,
+    estado int not null,
+	usuario varchar(250) not null,
+	fecha_modificacion datetime default current_timestamp,
+	primary key(id_categoria_auditoria)
+);
--- triggers.sql
+++ triggers.sql
@@ -5,7 +5,7 @@ drop trigger if exists on_update_cliente_auditoria;
 create or replace trigger on_update_cliente_auditoria before update on cliente
 for each row 
 begin 
-	insert into cliente_auditoria(fk_cliente, cedula, nombre, contacto, estado, usuario)
+	insert into cliente_auditoria(id_cliente, cedula, nombre, contacto, estado, usuario)
 	values (old.id_cliente, old.cedula, old.nombre, old.contacto, old.estado, user());
 end
 
