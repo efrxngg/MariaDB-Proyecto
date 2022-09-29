8db3d7dc4ad8031165c4d2f0f90241af2d6bd54e
gianpierre <113318718+gianpierre01@users.noreply.github.com>
2022-09-29 12:26:39 -0500

    querys.v1

--- querys.sql
+++ querys.sql
@@ -16,6 +16,24 @@ select df.fk_producto, df.cantidad, df.total  from det_factura as df inner join 
 
 
 -- COMPARACION 
+drop procedure if exists detalle_pedido_productos;
+create or replace procedure detalle_pedido_productos ( in id int)
+begin 
+	select     
+		dp.fk_producto  as producto , 
+		count (dp.fk_producto) as cantidad,  
+		pcp.precio as "prec/unid", 
+		(count (dp.fk_producto)* pcp.precio ) as total 
+	from det_pedido as dp
+		inner join precio_catalogo_producto as pcp on dp.fk_producto = pcp.id_prec_cata_prod  where dp.fk_cab_pedido = id
+		group by dp.fk_producto; 
+end
+
+call detalle_pedido_productos(1);
+select df.fk_producto, df.cantidad, df.total  from det_factura as df inner join cab_factura as cf on df.fk_cab_factura = cf.id_cab_factura  where cf.fk_cab_pedido = 1;
+
+
+-- COMPARACION 
 create or replace procedure compare_change(in id_cab_factura int)
 begin 
  	declare var_producto integer;
